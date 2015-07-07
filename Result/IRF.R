ff4e31edd5dd10df0f15dc7fcdb8d3be9ad74f51
clementcarrier <clement.carrier@ensae.fr>
2015-07-07 11:30:55 +0200

    maj

--- Result/IRF.R
+++ Result/IRF.R
@@ -5,21 +5,16 @@ require(urca)
 require(MSBVAR)
 
 
-#load("vardata3.Rdata")
+#load("Result/vardata3.Rdata")
 
 #data<-subset(vardataframe[113:176,])
 #datats<-ts(data,frequency=4, start=1998)
 
-load("Result/vardata")
-
+load("Result/vardata2")
 data<-subset(vardataframe[117:180,])
 datats<-ts(data,frequency=4, start=1998)
 
-difdata <- tail(data,-1) - head(data,-1)
-
-
-
-
+#difdata <- tail(data,-1) - head(data,-1)
 
 
 # exogenous shock
@@ -57,7 +52,7 @@ var1<-IRF[,1:5]
 var2<-IRF[,6:10]
 var3<-IRF[,11:15]
 var4<-IRF[,16:20]
-var5<-IRF[,21:25]
+var5<-IRF[,21:26]
 
 var1$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
 var2$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
@@ -89,7 +84,7 @@ rw<-function(t,x){
   y<-matrix(0,dim(x)[2],t)
   y[,1]<-t(x)
   for(i in 2:t){
-    y[,i] <- y[,i-1] + matrix(rnorm(dim(x)[2],0,0),dim(x)[2],1)
+    y[,i] <- y[,i-1] + matrix(rnorm(dim(x)[2],0,0.4942424),dim(x)[2],1)
   }
   return(y)
 }
@@ -122,7 +117,7 @@ var1<-IRF[,1:5]
 var2<-IRF[,6:10]
 var3<-IRF[,11:15]
 var4<-IRF[,16:20]
-var5<-IRF[,21:25]
+var5<-IRF[,21:26]
 
 var1$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
 var2$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
@@ -145,6 +140,21 @@ ggplot(mvar5, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="fr
 
 
 
+### inflation forecast with replication of rw
+
+iter<-100
+HICPpred<-matrix(0,13,iter)
+for (i in 1:iter){
+  HICPpred[,i]<-matrix(conditional(exo,end,1,12)[,"HICP"])
+}
+HICPpred
+HICPpred<-data.frame(HICPpred)
+HICPpred$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
+
+var <- melt(HICPpred,  id = 'time', variable.name = 'series')
+
+
+ggplot(var, aes(time,value, col=series)) + geom_point() + stat_smooth() 
 
 
 
@@ -152,36 +162,38 @@ ggplot(mvar5, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="fr
 
 
 
-lv<-lassovar(data,lags=1)
-lv$coef
-summary(lv)
-forecast<-forecast.lassovar(data,ntrain=51,horizon=1)
-summary(forecast)
-pred<-forecast$pred
-colnames(pred)<-names(data)
-pred<-data.frame(pred)
+# inflation rate
+HICP<-vardataframe[116:180,6]
+
+inflation<-NULL
+a<-NULL
+for (i in seq(from=1,to=length(HICP),by=1)){
+  inflation[i]=-1+exp(HICP[i])/exp(HICP[i-1])
+}
+inflation
+inflation<-inflation[-1]*100
+
+data$HICP<-NULL
+data$inflation<-inflation
+
+exo<-data.frame(data$POILU)
+colnames(exo)<-'POILU'
+end<-subset(data[,-which(names(data) %in% c("POILU"))])
 
 
-var1<-pred[,1:5]
-var2<-pred[,6:10]
-var3<-pred[,11:15]
-var4<-pred[,16:20]
-var5<-pred[,21:26]
+iter<-100
+HICPpred<-matrix(0,13,iter)
+for (i in 1:iter){
+  HICPpred[,i]<-matrix(conditional(exo,end,1,12)[,"inflation"])
+}
+HICPpred
+HICPpred<-data.frame(HICPpred)
+HICPpred$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
 
-var1$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
-var2$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
-var3$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
-var4$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
-var5$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
+var <- melt(HICPpred,  id = 'time', variable.name = 'series')
 
-mvar1 <- melt(var1,  id = 'time', variable.name = 'series')
-mvar2 <- melt(var2,  id = 'time', variable.name = 'series')
-mvar3 <- melt(var3,  id = 'time', variable.name = 'series')
-mvar4 <- melt(var4,  id = 'time', variable.name = 'series')
-mvar5 <- melt(var5,  id = 'time', variable.name = 'series')
 
-ggplot(mvar1, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar2, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar3, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar4, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar5, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
+ggplot(var, aes(time,value, col=series)) + geom_point() + stat_smooth() 
+
+
+
