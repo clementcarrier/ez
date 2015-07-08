a6e245861b97f58347f11f8dc161cd5b21e4e061
clementcarrier <clement.carrier@ensae.fr>
2015-07-08 13:43:41 +0200

    maj importante de la fonction RW

--- Result/IRF.R
+++ Result/IRF.R
@@ -10,11 +10,137 @@ require(MSBVAR)
 #data<-subset(vardataframe[113:176,])
 #datats<-ts(data,frequency=4, start=1998)
 
-load("vardata2")
+load("Result/vardata2")
 data<-subset(vardataframe[117:180,])
 datats<-ts(data,frequency=4, start=1998)
 
 #difdata <- tail(data,-1) - head(data,-1)
+
+
+
+
+##############################
+
+# generate RW exogonous variables
+
+library(base)
+
+#la matrice d'entrée est une matrice avec en ligne les périodes et en colonnes les variables
+
+rw<-function(t,x){
+  y<-matrix(0,dim(x)[2],t)
+  y[,1]<-as.matrix(exo[dim(x)[1],])
+  if(dim(exo)[2]==1) {
+    for(i in 2:t){
+      y[,i] <- y[,i-1] + matrix(mvrnorm(n = 1, mu=rep(0,dim(x)[2]) , Sigma=var(x)),dim(x)[2],1)
+    }
+  }
+  else {
+    for(i in 2:t){
+      y[,i] <- y[,i-1] + matrix(mvrnorm(n = 1, mu=rep(0,dim(x)[2]) , Sigma=diag(diag(var(x)))),dim(x)[2],1)
+    }
+  }
+  return(y[,-1])
+}
+
+
+conditional<-function(exogen,data,lag,horizon,preforecast){
+  all=data.frame(exogen,data)
+  fore<-matrix(0,nrow=dim(data)[2]+dim(exogen)[2],ncol=horizon+preforecast)
+  fore[,1:(preforecast)]<-t(all[(dim(all)[1]-preforecast+1):dim(all)[1],])
+  fore[1:dim(exogen)[2],-c(1:(preforecast))]<-rw(horizon+1,exo)
+  lv<-lassovar(dat=all,lags=lag, ic="BIC")
+  coeff<-as.matrix(t(lv$coefficients[-1,]),26,26)
+  intercept<-as.matrix(lv$coefficients[1,],26,1)
+  for (i in (preforecast+1):(horizon+preforecast)){
+    fore[-dim(exogen)[2],i]<-intercept[-dim(exogen)[2],]+(coeff%*%fore[,i-1])[-dim(exogen)[2]]
+  }
+  rownames(fore)<-names(all)
+  return(t(fore))
+}
+
+
+
+
+###### inflation rate ######
+HICP<-vardataframe[113:180,6]
+
+inflation<-NULL
+for (i in seq(from=5,to=length(HICP),by=1)){
+  inflation[i]=-1+exp(HICP[i])/exp(HICP[i-4])
+}
+inflation<-inflation[5:68]*100
+
+data$HICP<-NULL
+data$inflation<-inflation
+
+exo<-as.matrix(data[,c("DJES")])
+colnames(exo)<-c('DJES')
+end<-subset(data[,-which(names(data) %in% c("DJES"))])
+
+
+iter<-100
+preforecast<-16
+horizon<-28
+
+HICPpred<-matrix(0,horizon+preforecast,iter)
+for (i in 1:iter){
+  HICPpred[,i]<-matrix(conditional(exo,end,1,horizon,preforecast)[,"inflation"])
+}
+HICPpred
+HICPpred<-data.frame(HICPpred)
+HICPpred$time<-seq(as.Date("2010/01/01"), as.Date("2020/12/31"), by = "quarter")
+
+var <- melt(HICPpred,  id = 'time', variable.name = 'series')
+
+
+ggplot(var, aes(time,value, col=series)) + geom_point() 
+plot(inflation)
+
+
+
+
+
+
+
+######################################################
+exo<-as.matrix(data[,c("POILU")])
+colnames(exo)<-'POILU'
+end<-subset(data[,-which(names(data) %in% c("POILU"))])
+
+
+IRF<-conditional(exo,end,1,12,4)
+IRF<-data.frame(IRF)
+
+var1<-IRF[,1:5]
+var2<-IRF[,6:10]
+var3<-IRF[,11:15]
+var4<-IRF[,16:20]
+var5<-IRF[,21:26]
+
+time<-seq(as.Date("2010/01/01"), as.Date("2016/12/31"), by = "quarter")
+
+var1$time<-time
+var2$time<-time
+var3$time<-time
+var4$time<-time
+var5$time<-time
+
+mvar1 <- melt(var1,  id = 'time', variable.name = 'series')
+mvar2 <- melt(var2,  id = 'time', variable.name = 'series')
+mvar3 <- melt(var3,  id = 'time', variable.name = 'series')
+mvar4 <- melt(var4,  id = 'time', variable.name = 'series')
+mvar5 <- melt(var5,  id = 'time', variable.name = 'series')
+
+ggplot(mvar1, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
+ggplot(mvar2, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
+ggplot(mvar3, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
+ggplot(mvar4, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
+ggplot(mvar5, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
+
+
+
+
 
 
 # exogenous shock
@@ -73,137 +199,5 @@ ggplot(mvar4, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="fr
 ggplot(mvar5, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
 
 
-
-
-##############################
-
-# generate RW exogonous variables
-
-#variance du bruit ?
-
-rw<-function(t,x){
-  y<-matrix(0,dim(x)[2],t)
-  y[,1]<-t(x)
-  for(i in 2:t){
-    y[,i] <- y[,i-1] + matrix(rnorm(dim(x)[2],0,0.5),dim(x)[2],1)
-  }
-  return(y[,-1])
-}
-
-
-
-
-
-conditional<-function(exogen,data,lag,horizon,preforecast){
-  all=data.frame(exogen,data)
-  fore<-matrix(0,nrow=dim(data)[2]+dim(exogen)[2],ncol=horizon+preforecast)
-  fore[,1:(preforecast)]<-t(all[(dim(all)[1]-preforecast+1):dim(all)[1],])
-  fore[1:dim(exogen)[2],-c(1:(preforecast))]<-rw(horizon+1,as.matrix(exogen[dim(exogen)[1],]))
-  lv<-lassovar(dat=all,lags=lag, ic="BIC")
-  coeff<-as.matrix(t(lv$coefficients[-1,]),26,26)
-  intercept<-as.matrix(lv$coefficients[1,],26,1)
-  for (i in (preforecast+1):(horizon+preforecast)){
-    fore[-dim(exogen)[2],i]<-intercept[-dim(exogen)[2],]+(coeff%*%fore[,i-1])[-dim(exogen)[2]]
-  }
-  rownames(fore)<-names(all)
-  return(t(fore))
-}
-
-exo<-data.frame(data$POILU)
-colnames(exo)<-'POILU'
-end<-subset(data[,-which(names(data) %in% c("POILU"))])
-
-
-IRF<-conditional(exo,end,1,12,4)
-IRF<-data.frame(IRF)
-
-var1<-IRF[,1:5]
-var2<-IRF[,6:10]
-var3<-IRF[,11:15]
-var4<-IRF[,16:20]
-var5<-IRF[,21:26]
-
-time<-seq(as.Date("2010/01/01"), as.Date("2016/12/31"), by = "quarter")
-
-var1$time<-time
-var2$time<-time
-var3$time<-time
-var4$time<-time
-var5$time<-time
-
-mvar1 <- melt(var1,  id = 'time', variable.name = 'series')
-mvar2 <- melt(var2,  id = 'time', variable.name = 'series')
-mvar3 <- melt(var3,  id = 'time', variable.name = 'series')
-mvar4 <- melt(var4,  id = 'time', variable.name = 'series')
-mvar5 <- melt(var5,  id = 'time', variable.name = 'series')
-
-ggplot(mvar1, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar2, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar3, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar4, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-ggplot(mvar5, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
-
-
-
-
-### inflation forecast with replication of rw
-
-iter<-100
-HICPpred<-matrix(0,28,iter)
-for (i in 1:iter){
-  HICPpred[,i]<-matrix(conditional(exo,end,1,12)[,"HICP"])
-}
-HICPpred
-HICPpred<-data.frame(HICPpred)
-HICPpred$time<-seq(as.Date("2014/10/01"), as.Date("2017/12/31"), by = "quarter")
-
-var <- melt(HICPpred,  id = 'time', variable.name = 'series')
-
-
-ggplot(var, aes(time,value, col=series)) + geom_point() + stat_smooth() 
-
-
-
-
-
-
-
-
-
-
-###### inflation rate ######
-HICP<-vardataframe[113:180,6]
-
-inflation<-NULL
-for (i in seq(from=5,to=length(HICP),by=1)){
-  inflation[i]=-1+exp(HICP[i])/exp(HICP[i-4])
-}
-inflation
-inflation<-inflation[5:68]*100
-
-data$HICP<-NULL
-data$inflation<-inflation
-
-exo<-data.frame(data$POILU)
-colnames(exo)<-'POILU'
-end<-subset(data[,-which(names(data) %in% c("POILU"))])
-
-
-iter<-1000
-preforecast<-16
-horizon<-12
-
-HICPpred<-matrix(0,horizon+preforecast,iter)
-for (i in 1:iter){
-  HICPpred[,i]<-matrix(conditional(exo,end,1,horizon,preforecast)[,"inflation"])
-}
-HICPpred
-HICPpred<-data.frame(HICPpred)
-HICPpred$time<-seq(as.Date("2010/01/01"), as.Date("2016/12/31"), by = "quarter")
-
-var <- melt(HICPpred,  id = 'time', variable.name = 'series')
-
-
-ggplot(var, aes(time,value, col=series)) + geom_point() 
 
 
